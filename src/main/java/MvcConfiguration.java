a55b1cc4f8ea4ac3d965ceea39f951d1368df6bc
Herick Oliveira <herick-mo@sefaz.go.gov.br>
2014-03-17 17:32:15 -0300

    First commit.

---
+++ src/main/java/MvcConfiguration.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
